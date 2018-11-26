e06a9cb2b563fff61b4c24c892c6f7d0eb6e6819
Kyle Gellatly <kyle.gellatly@umassmed.edu>
2018-11-26 13:03:35 -0500

    Updated to fix cas9
    still need to work on CPF1 distances....

--- CRISPR_Designed.R
+++ CRISPR_Designed.R
@@ -1,6 +1,6 @@
 library(Biostrings)
 
-find_guides <- function(input, nuclease_table, stringency = 5){
+find_guides <- function(input, nuclease_table, stringency = 5, remove_wt = T){
   input2 <- as.character(unlist(input$V2)) # I hate factors... should just take your input and turn it into a vector
   dat <- matrix(ncol = length(input2), nrow = nchar(input2)[1]) # Now we make a dataframe
   colnames(dat) <- input$V1
@@ -9,7 +9,7 @@ find_guides <- function(input, nuclease_table, stringency = 5){
     int <- input2[i]
     dat[,i] <- unlist(strsplit(int, split = ""))
   }
-
+  
   # Now we query WHERE in our sequence a SNP occurs
   if(ncol(dat) > 1){
     positions <- apply(dat,1,unique)
@@ -23,7 +23,7 @@ find_guides <- function(input, nuclease_table, stringency = 5){
   } else {
     mut_pos <- seq(1:nrow(dat))
   }
-
+  
   #Forward
   guides_f <- vector(mode = "list", length = nrow(nuclease_table))
   for (i in 1:nrow(nuclease_table)) {
@@ -45,6 +45,9 @@ find_guides <- function(input, nuclease_table, stringency = 5){
           for (l in 1:length(good)) {
             goodint <- good[l]
             cut_dist <- start_pos_mut[good][l]
+            if(cut_dist > 0){
+              cut_dist <- cut_dist-1
+            }
             int_new <- occurances[goodint]
             if(int_nuc$Cut < 0){
               if(start(int_new)-int_nuc$Guide.Length > 0){
@@ -82,7 +85,7 @@ find_guides <- function(input, nuclease_table, stringency = 5){
       }
     }
   }
-
+  
   #Reverse
   guides_r <- vector(mode = "list", length = nrow(nuclease_table))
   for (i in 1:nrow(nuclease_table)) {
@@ -99,13 +102,14 @@ find_guides <- function(input, nuclease_table, stringency = 5){
       start_pos_cut <- start_pos + int_nuc$Cut # FIND CUT POSITION
       for (k in 1:length(mut_pos)) {
         int_mut_pos <- mut_pos[k]
-        int_mut_pos <- length(dat[,1]) - int_mut_pos
+        int_mut_pos <- length(dat[,1]) - int_mut_pos +1
         start_pos_mut <- start_pos_cut - int_mut_pos # DISTANCE OF CUT POSITION FROM MUTANT POSITIONS
         good <- which(abs(start_pos_mut)<=stringency) # WHICH ARE CLOSER THAN STRINGENCY
         if(length(good >= 1)){
           for (l in 1:length(good)) {
             goodint <- good[l]
             cut_dist <- start_pos_mut[good][l]
+            cut_dist <- cut_dist-1
             int_new <- occurances[goodint]
             if(int_nuc$Cut < 0){
               if(start(int_new)-int_nuc$Guide.Length > 0){
@@ -143,7 +147,7 @@ find_guides <- function(input, nuclease_table, stringency = 5){
       }
     }
   }
-
+  
   full_guides <- mapply(c, guides_f, guides_r, SIMPLIFY=FALSE)
   names(full_guides) <- nuclease_table$Name
   guide_table <- matrix(nrow = length(unlist(full_guides)))
@@ -153,12 +157,12 @@ find_guides <- function(input, nuclease_table, stringency = 5){
   guide_table[,3] <- NA
   guide_table[match(unlist(guides_f), as.character(guide_table[,1])),3] <- "+"
   guide_table[match(unlist(guides_r), as.character(guide_table[,1])),3] <- "-"
-
+  
   guide_table[,4] <- matrix(unlist(strsplit(as.character(guide_table[,1]), split = "_")), ncol = 5, byrow = T)[,2]
   guide_table[,5] <- matrix(unlist(strsplit(as.character(guide_table[,1]), split = "_")), ncol = 5, byrow = T)[,3]
   guide_table[,6] <- matrix(unlist(strsplit(as.character(guide_table[,1]), split = "_")), ncol = 5, byrow = T)[,4]
   guide_table[,7] <- matrix(unlist(strsplit(as.character(guide_table[,1]), split = "_")), ncol = 5, byrow = T)[,5]
-
+  
   for (i in 1:nrow(guide_table)) {
     for (j in 1:length(unique(nuclease_table)$Name)) {
       int <- guide_table[i,"V2"]
@@ -170,36 +174,40 @@ find_guides <- function(input, nuclease_table, stringency = 5){
       }
     }
   }
-
- if(ncol(dat) == 1){
-   trimmed_guides <- guide_table
-   trimmed_guides[,1] <- matrix(unlist(strsplit(as.character(guide_table[,1]), split = "_")), ncol = 5, byrow = T)[,1]
-   trimmed_guides <- trimmed_guides[,-4]
-   unique_guides <- unique(trimmed_guides[,1])
-   trimmed_guides2 <- trimmed_guides[match(unique_guides, trimmed_guides[,1]),]
-   trimmed_guides3 <- trimmed_guides2[,c(1,4,2,3)]
-   colnames(trimmed_guides3) <- c("Spacer_seq", "PAM", "Nuclease", "Strand")
-   trimmed_guides <- trimmed_guides3
-   rownames(trimmed_guides) <- seq(1:nrow(trimmed_guides))
- } else {
-   trimmed_guides <- guide_table
-   trimmed_guides[,1] <- matrix(unlist(strsplit(as.character(guide_table[,1]), split = "_")), ncol = 5, byrow = T)[,1]
-   trimmed_guides[,8] <- paste0(trimmed_guides[,1], trimmed_guides[,5])
-   unique_guides <- unique(trimmed_guides[,8])
-   trimmed_guides2 <- trimmed_guides[match(unique_guides, trimmed_guides[,8]),]
-   for (i in 1:nrow(trimmed_guides2)){
-     int <- trimmed_guides2[i,"V8"]
-     ind <- grep(paste0("^", int, "$"), trimmed_guides$V8)
-     name <- paste0(trimmed_guides$V4[ind], collapse = "_")
-     trimmed_guides2$V4[i] <- name
-   }
-   trimmed_guides <- trimmed_guides2
-   trimmed_guides <- trimmed_guides[,c(1,5,2,7,6,3,4)]
-   colnames(trimmed_guides) <- c("Spacer_seq", "PAM", "Nuclease", "SNP_PAM", "Cut_SNP", "Strand", "Genotype")
-   rownames(trimmed_guides) <- seq(1:nrow(trimmed_guides))
- }
+  
+  if(ncol(dat) == 1){
+    trimmed_guides <- guide_table
+    trimmed_guides[,1] <- matrix(unlist(strsplit(as.character(guide_table[,1]), split = "_")), ncol = 5, byrow = T)[,1]
+    trimmed_guides <- trimmed_guides[,-4]
+    unique_guides <- unique(trimmed_guides[,1])
+    trimmed_guides2 <- trimmed_guides[match(unique_guides, trimmed_guides[,1]),]
+    trimmed_guides3 <- trimmed_guides2[,c(1,4,2,3)]
+    colnames(trimmed_guides3) <- c("Spacer_seq", "PAM", "Nuclease", "Strand")
+    trimmed_guides <- trimmed_guides3
+    rownames(trimmed_guides) <- seq(1:nrow(trimmed_guides))
+  } else {
+    trimmed_guides <- guide_table
+    trimmed_guides[,1] <- matrix(unlist(strsplit(as.character(guide_table[,1]), split = "_")), ncol = 5, byrow = T)[,1]
+    trimmed_guides[,8] <- paste0(trimmed_guides[,1], trimmed_guides[,5])
+    unique_guides <- unique(trimmed_guides[,8])
+    trimmed_guides2 <- trimmed_guides[match(unique_guides, trimmed_guides[,8]),]
+    for (i in 1:nrow(trimmed_guides2)){
+      int <- trimmed_guides2[i,"V8"]
+      ind <- grep(paste0("^", int, "$"), trimmed_guides$V8)
+      name <- paste0(trimmed_guides$V4[ind], collapse = "_")
+      trimmed_guides2$V4[i] <- name
+    }
+    trimmed_guides <- trimmed_guides2
+    trimmed_guides <- trimmed_guides[,c(1,5,2,7,6,3,4)]
+    colnames(trimmed_guides) <- c("Spacer_seq", "PAM", "Nuclease", "SNP_PAM", "Cut_SNP", "Strand", "Genotype")
+    rownames(trimmed_guides) <- seq(1:nrow(trimmed_guides))
+  }
+  if(remove_wt == T){
+    remov_ind <- grep(paste0("^", "WT", "$"), trimmed_guides$Genotype)
+    trimmed_guides <- trimmed_guides[-remov_ind,]
+  }
   return(trimmed_guides)
 }
 
-guides <- find_guides(input = mutant_data, nuclease_table = CRISPR_Nuclease_Table, stringency = 5)
-guides
+guides <- find_guides(input = IVS_II_654, nuclease_table = CRISPR_Nuclease_Table, stringency = 6)
+write.csv(guides, file = "guides.csv")
